261e63ea495d80d24232f64aa11c39fd6bc4386c
puvvadamaniharish <41737685+puvvadamaniharish@users.noreply.github.com>
2018-07-31 11:10:15 +0530

    Create dispaly1.java

---
+++ dispaly1.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	
+	              int n,i,count=0,k;
+	              int a[] = new int[10];
+	              Scanner s = new Scanner (System.in);
+	              n = s.nextInt();
+	              k = s.nextInt();
+	              for (i=0;i<n;i++)
+	              {
+	            	a[i] = s.nextInt();
+	              }
+	              for ( i =0;i<k;i++)
+	              {
+	              	 count = count + a[i];
+	              }
+	              System.out.println(count);
+	  	}
+}
